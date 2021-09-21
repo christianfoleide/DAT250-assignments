37f771cdf2310ca46d90f70d4b1176a9fe0f83d5
christianfoleide <christian.foleide@gmail.com>
2021-09-21 20:24:15 +0200

    start assignment 4

---
+++ jpa-basic/src/main/java/no/hvl/dat250/spark/Main.java
@@ -0,0 +1,9 @@
+package no.hvl.dat250.spark;
+
+public class Main {
+    private static final String PERSISTENCE_UNIT_NAME = "feedapp"; // "people"
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ jpa-basic/src/main/java/no/hvl/dat250/spark/entities/Device.java
@@ -0,0 +1,8 @@
+package no.hvl.dat250.spark.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Device {
+
+}
