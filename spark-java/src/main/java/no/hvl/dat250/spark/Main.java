6098cc728eec80f8ed4a2e3cc74d7277aa3e49e7
christianfoleide <christian.foleide@gmail.com>
2021-09-21 22:58:19 +0200

    remove temp folder

--- spark-java/src/main/java/no/hvl/dat250/spark/Main.java
+++
@@ -1,9 +0,0 @@
-package no.hvl.dat250.spark;
-
-public class Main {
-    private static final String PERSISTENCE_UNIT_NAME = "feedapp"; // "people"
-
-    public static void main(String[] args) {
-
-    }
-}
--- spark-java/src/main/java/no/hvl/dat250/spark/entities/Device.java
+++
@@ -1,8 +0,0 @@
-package no.hvl.dat250.spark.entities;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Device {
-
-}
